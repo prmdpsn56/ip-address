16dd3cfc26cc12bdb84491198fdc0f6cba001316
Param <user@users-MacBook-Pro.local>
2022-12-04 15:36:27 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ip-address';
+  ipAddress = '';
+  
+  constructor(private http:HttpClient) { }
+  
+  ngOnInit() {
+      this.getIPAddress();
+  }
+  
+  getIPAddress()
+  {
+    this.http.get("http://api.ipify.org/?format=json").subscribe((res:any)=>{
+      this.ipAddress = res.ip;
+    });
+  }
 }
