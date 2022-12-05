24b03b7781bedabb7449613c21da6414a912dcb9
Param <user@users-MacBook-Pro.local>
2022-12-04 17:38:09 -0800

    git add

--- dist/ip-address/main.js
+++ dist/ip-address/main.js
@@ -81,8 +81,8 @@ class AppComponent {
         this.getIPAddress();
     }
     getIPAddress() {
-        this.http.get("http://api.ipify.org/?format=json").subscribe((res) => {
-            this.ipAddress = res.ip;
+        this.http.get("http://54.163.117.127:8080").subscribe((res) => {
+            console.log(res);
         });
     }
 }
